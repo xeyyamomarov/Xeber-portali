5a20daa989b242d052b41c3176db79eb3dfd5992
Elvin Mammadli <mamedli.elvin@gmail.com>
2022-08-26 20:46:47 +0400

    business

--- src/components/CardItem/BusinessItem.jsx
+++ src/components/CardItem/BusinessItem.jsx
@@ -1,13 +1,24 @@
 import "./style.css";
 import { BsClock, BsFillPersonFill } from "react-icons/bs";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const BusinessItem = ({ business }) => {
+  const [businessData, setBusinessData] = useState([])
+  const navigate = useNavigate();
+
   const { id } = useParams();
-  return (
-    <div>
-      <div className="main-item">
-        {business?.filter((card) => card.id === id)?.map((card) => {
+
+  useEffect(() => {
+    setBusinessData(business)
+  }, [business])
+
+  let x = businessData.filter((card) => card.id === id)
+  if (x.length) {
+    return (
+      <div>
+        <div className="main-item">
+          {business?.filter((card) => card.id === id)?.map((card) => {
             return (
               <div key={card.id} className="card-item">
                 <div className="img-item">
@@ -30,34 +41,37 @@ export const BusinessItem = ({ business }) => {
               </div>
             );
           })}
-      </div>
-      <div className="similar-news">
-        <div className="a">
-          <p>Similar news</p>
         </div>
+        <div className="similar-news">
+          <div className="a">
+            <p>Similar news</p>
+          </div>
 
-        <div className="data">
-          {business?.slice(0, 3).map((business) => {
-            return (
-              <div key={business.id} className="similar-card">
-                <div className="similar-content">
-                  <h1>{business.title}</h1>
-                </div>
-                <div className="footer-similar">
-                  <div className="icon">
-                    <BsClock />
-                    <span>{business.date}</span>
+          <div className="data">
+            {business?.slice(0, 3).map((business) => {
+              return (
+                <div key={business.id} className="similar-card">
+                  <div className="similar-content">
+                    <h1>{business.title}</h1>
                   </div>
-                  <div className="icon">
-                    <BsFillPersonFill />
-                    <span>{business.author}</span>
+                  <div className="footer-similar">
+                    <div className="icon">
+                      <BsClock />
+                      <span>{business.date}</span>
+                    </div>
+                    <div className="icon">
+                      <BsFillPersonFill />
+                      <span>{business.author}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    navigate("/")
+  }
 };
--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,10 +1,11 @@
 import "./style.css";
 import { BsClock, BsFillPersonFill } from "react-icons/bs";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export const CardItem = ({ cards }) => {
   const [cardsData, setCardsData] = useState([]);
+  const navigate = useNavigate();
   const { id } = useParams();
 
   useEffect(() => {
@@ -13,16 +14,51 @@ export const CardItem = ({ cards }) => {
 
   console.log(cardsData);
 
-  return (
-    <div>
-      <div className="main-item">
-        {/* {cardsData && cardsData.filter((card) => card.id === id)
-          .map((card) => {
-            return (
-              <div key={card.id} className="card-item">
-                <div className="img-item">
-                  <img src={card.imageUrl} alt="/" />
-                  <div className="footer-item">
+  let x = cardsData.filter((card) => card.id === id)
+console.log(x);
+  if(x.length) {
+    return (
+      <div>
+        <div className="main-item">
+          {cardsData && cardsData.filter((card) => card.id === id)
+            .map((card) => {
+              return (
+                <div key={card.id} className="card-item">
+                  <div className="img-item">
+                    <img src={card.imageUrl} alt="/" />
+                    <div className="footer-item">
+                      <div className="icon">
+                        <BsClock />
+                        <span>{card.date}</span>
+                      </div>
+                      <div className="icon">
+                        <BsFillPersonFill />
+                        <span>{card.author}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="content-item">
+                    <h1>{card.title}</h1>
+                    <p>{card.content}</p>
+                  </div>
+                </div>
+              );
+            })}
+            {cardsData && "salam"}
+        </div>
+        <div className="similar-news">
+          <div className="a">
+            <p>Similar news</p>
+          </div>
+  
+          <div className="data">
+            {cardsData && cardsData.slice(0, 3).map((card) => {
+              return (
+                <div key={card.id} className="similar-card">
+                  <div className="similar-content">
+                    <h1>{card.title}</h1>
+                  </div>
+                  <div className="footer-similar">
                     <div className="icon">
                       <BsClock />
                       <span>{card.date}</span>
@@ -33,42 +69,13 @@ export const CardItem = ({ cards }) => {
                     </div>
                   </div>
                 </div>
-                <div className="content-item">
-                  <h1>{card.title}</h1>
-                  <p>{card.content}</p>
-                </div>
-              </div>
-            );
-          })} */}
-          {cardsData && "salam"}
-      </div>
-      <div className="similar-news">
-        <div className="a">
-          <p>Similar news</p>
-        </div>
-
-        <div className="data">
-          {cardsData && cardsData.slice(0, 3).map((card) => {
-            return (
-              <div key={card.id} className="similar-card">
-                <div className="similar-content">
-                  <h1>{card.title}</h1>
-                </div>
-                <div className="footer-similar">
-                  <div className="icon">
-                    <BsClock />
-                    <span>{card.date}</span>
-                  </div>
-                  <div className="icon">
-                    <BsFillPersonFill />
-                    <span>{card.author}</span>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    navigate("/")
+  }
 };
