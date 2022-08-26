1faf87fa2aa83bf9874da55c327af525d551fe6b
unknown <xeyyamomarov2014@gmail.com>
2022-08-26 09:04:36 +0400

    /

--- src/components/Card/World.jsx
+++ src/components/Card/World.jsx
@@ -1,14 +1,20 @@
 import "./style.css";
 import { BsClock, BsFillPersonFill } from "react-icons/bs";
 import { MainContext, useContext } from "../Context";
+import { useNavigate } from "react-router-dom";
 export const World = () => {
   const {world}=useContext(MainContext)
+  const navigate=useNavigate()
   return (
     <div className="all-cards">
       <div className="main">
         {world.slice(0, 6).map((card) => {
           return (
-            <div className="card" key={card.id}>
+            <div  
+            onClick={() => {
+              navigate(`/world/${card.id}`);
+            }}
+            className="card" key={card.id}>
               <img className="img" src={card.imageUrl} alt="/" />
               <h1 className="title">{card.title}</h1>
               <p className="content">{card.content}</p>
